cd8f0976745baca47f2ec534097c31bb66eaa409
Sai Teja Kolagani <saiteja.kolagani@gmail.com>
2026-06-24 19:37:17 +0530

    Renamed the data-model.md to resources.md file and added the required links

--- ddl/02_customers.sql
+++ ddl/02_customers.sql
@@ -2,7 +2,7 @@
 CREATE TABLE ecommerce.customers (
     customer_id VARCHAR(50) PRIMARY KEY,
     customer_unique_id VARCHAR(50) NOT NULL,
-    customer_zip_code_prefix INTEGER,
+    customer_zip_code_prefix INTEGER NOT NULL,
     customer_city VARCHAR(100),
     customer_state CHAR(2)
 );
---
+++ docs/resources.md
@@ -0,0 +1,4 @@
+PostgreSQL Installation: https://www.postgresql.org/download/
+
+Kaggle Dataset: https://www.kaggle.com/datasets/olistbr/brazilian-ecommerce/data?select=olist_customers_dataset.csv
+
